49da00751865fc17214f463dc11abd2f7414926b
aurinex <zorrosqd02@gmail.com>
2024-09-19 10:47:46 +0500

    Add button later in UpdateNotification

--- src/renderer/components/UpdateNotification/UpdateNotification.tsx
+++ src/renderer/components/UpdateNotification/UpdateNotification.tsx
@@ -23,14 +23,28 @@ function UpdateNotification() {
     window.electron.ipcRenderer.sendMessage('restart-app'); // Отправляем событие на перезапуск
   };
 
+  const handleLater = () => {
+    setOpen(false);
+    // Устанавливаем таймер для повторного показа через час (3600000 мс = 1 час)
+    setTimeout(() => {
+      setOpen(true);
+    }, 3600000);
+  };
+
   return (
     <Snackbar
+    autoHideDuration={600000}
       open={open}
       message="Обновление загружено. Перезагрузите приложение для применения обновлений."
       action={
-        <Button color="primary" size="small" onClick={handleRestart}>
-          Перезагрузить
-        </Button>
+        <>
+          <Button color="primary" size="small" onClick={handleRestart}>
+            Перезагрузить
+          </Button>
+          <Button color="secondary" size="small" onClick={handleLater}>
+            Позже
+          </Button>
+        </>
       }
       onClose={handleClose}
     />
