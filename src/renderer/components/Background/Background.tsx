e16e2016f9e8a272fe76c3b256e6e1ed7cbed24b
reMud <killersan0010@gmail.com>
2024-11-18 19:59:20 +0500

    fix background: add infinite animation, del component

--- src/renderer/components/Background/Background.tsx
+++ src/renderer/components/Background/Background.tsx
@@ -3,52 +3,45 @@ import { IconButton } from '@mui/material';
 import { useState } from 'react';
 import ContrastIcon from '@mui/icons-material/Contrast';
 import BackgroundCubics from './Backgrounds/Background-cubics';
-import BackgroundCircle from './Backgrounds/Background-circle';
 import BackgroundGradient from './Backgrounds/Background-gradient';
-import BackGroundWave from './Backgrounds/Background_wave';
+//import BackGroundWave from './Backgrounds/Background_wave';
+//import BackgroundCircle from './Backgrounds/Background-circle';
 
-const Backgrounds: JSX.Element[] = [
+const BACKGROUNDS = [
+  //<BackgroundCircle />, needs optimization
+  //<BackGroundWave />, needs fixing: rendering doesn't complete when component is removed
   <BackgroundCubics />,
-  // requires optimization
-  //<BackgroundCircle />,
-  <BackGroundWave />,
-  <BackgroundGradient />,
+  <BackgroundGradient />
 ];
 
 function Background() {
-  const [iBackground, setIBackground] = useState(
-    Math.floor(Math.random() * (Backgrounds.length-1))
-  );
+  const [currentIndex, setCurrentIndex] = useState(Math.floor(Math.random() * BACKGROUNDS.length));
 
-  function nextBackground() {
-    let temp = iBackground + 1;
-
-    if (temp >= Backgrounds.length) {
-      temp = 0;
-    }
-
-    setIBackground(temp);
-  }
+  const handleBackgroundChange = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % BACKGROUNDS.length);
+  };
 
   return (
     <div className="Background">
       <IconButton
         aria-label="Theme"
         size="large"
-        onClick={nextBackground}
+        onClick={handleBackgroundChange}
         className="Background__Button-theme"
         sx={{
           borderRadius: '100%',
-          border: 'solid 1px black',
-          borderColor: 'rgba(0, 0, 0, 0.12)',
-          boxShadow:
-            '0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)',
-          backgroundColor: 'rgba(255, 255, 255, 0.4)',
+          border: '1px solid rgba(0, 0, 0, 0.12)',
+          boxShadow: '0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)',
+          backgroundColor: 'rgba(255, 255, 255, 0.4)'
         }}
       >
         <ContrastIcon />
       </IconButton>
-      <div className="Background__Background">{Backgrounds[iBackground]}</div>
+      <div className="Background__Background">
+        <div style={{position: 'absolute', width: '100%', height: '100%'}}>
+          {BACKGROUNDS[currentIndex]}
+        </div>
+      </div>
     </div>
   );
 }
